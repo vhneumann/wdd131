c5cf76466dae01aca39e57ba52404772961c4c9b
Victor Neumann <vhneumann@gmail.com>
2026-05-21 01:16:58 -0300

    Added javascript to BOM.

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,1 +1,20 @@
 // here goes the code for the BOM page.
+const input = document.queryselector('#favchap');
+const button = document.queryselector('button');
+const list = document.queryselector('list');
+const li = document.queryselector('li');
+const deleteButton = document.queryselector('button');
+li.textContent = input.value;
+deleteButton.textContent = 'X';
+li.append(deleteButton);
+list.append(li);
+button.addEventListener('click', function() {
+
+});
+
+if (input.value.trim() !== '') {};
+
+deleteButton.addEventListener('click', function() {
+    list.removeChild(li);
+    input.focus();
+});
