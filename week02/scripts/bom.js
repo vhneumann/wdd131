2c94104c0f30a129102d169509714ee2c17dd3f1
Victor Neumann <vhneumann@gmail.com>
2026-09-17 19:59:27 -0300

    W02 DOM and Javascript 1

---
+++ week02/scripts/bom.js
@@ -0,0 +1,15 @@
+const input = document.querySelector('#favchap');
+const button = document.querySelector('button');
+const list = document.querySelector('li');
+
+const li = document.createElement('li');
+
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value;
+
+deleteButton.textContent = "❌";
+
+li.append(deleteButton);
+
+list.append(li);
